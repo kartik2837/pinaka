5e54c8db9c013e6307f35d1198bc26fad4bb29a7
Kartik Bhadouriya <ks8783375@gmail.com>
2025-06-20 16:23:27 +0530

    Update Poster.jsx

--- src/Components/Poster/Poster.jsx
+++ src/Components/Poster/Poster.jsx
@@ -1,35 +1,35 @@
 
 
-// import Posterimg from '/src/assets/Banner/poster.jpg';
-
-// const Poster = () => {
-//   return (
-//     <div className="w-full overflow-hidden rounded-xl">
-//       <img
-//         src={Posterimg}
-//         alt="Banner"
-//         className="w-full h-auto sm:h-96 md:h-48 lg:h-80 xl:h-[700px] object-cover object-center"
-//       />
-//     </div>
-//   );
-// };
-
-// export default Poster;
-
-
-
 import Posterimg from '/src/assets/Banner/poster.jpg';
 
 const Poster = () => {
   return (
-    <div className="w-full overflow-hidden rounded-xl aspect-[16/9] md:aspect-auto">
+    <div className="w-full overflow-hidden rounded-xl">
       <img
         src={Posterimg}
         alt="Banner"
-        className="w-full h-full object-cover object-center md:h-48 lg:h-80 xl:h-[700px]"
+        className="w-full h-auto sm:h-96 md:h-48 lg:h-80 xl:h-[700px] object-cover object-center"
       />
     </div>
   );
 };
+
+export default Poster;
+
+
+
+// import Posterimg from '/src/assets/Banner/poster.jpg';
+
+// const Poster = () => {
+//   return (
+//     <div className="w-full overflow-hidden rounded-xl aspect-[16/9] md:aspect-auto">
+//       <img
+//         src={Posterimg}
+//         alt="Banner"
+//         className="w-full h-full object-cover object-center md:h-48 lg:h-80 xl:h-[700px]"
+//       />
+//     </div>
+//   );
+// };
 
 export default Poster
